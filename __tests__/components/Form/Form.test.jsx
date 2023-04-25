d0cc3005490aff62c20825174991eab290d3679a
LuisGerezM <luis.gerez@gmail.com>
2023-04-25 10:56:00 -0300

    ADD: Agregando algo de test al form

--- __tests__/components/Form/Form.test.jsx
+++ __tests__/components/Form/Form.test.jsx
@@ -1,14 +1,15 @@
+import ClientWalletForm from "@/components/Form";
 import {
-  render,
-  screen,
   cleanup,
+  render,
   renderHook,
+  screen,
   waitFor
 } from "@testing-library/react";
+import { FormProvider, useForm } from "react-hook-form";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-import ClientWalletForm from "@/components/Form";
-import { FormProvider, useForm } from "react-hook-form";
+import { mockOnClick } from "../../../.jest/mocks/onClickButton.mock";
 
 const getUseFormMock = defaultValue =>
   renderHook(() =>
@@ -19,12 +20,17 @@ const getUseFormMock = defaultValue =>
     })
   );
 
+jest.mock("@/components/ErrorsValidationForm/index.jsx", () => ({
+  __esModule: true,
+  default: () => (
+    <div data-testid='ErrorsValidationForm'>ErrorsValidationForm</div>
+  )
+}));
+
 describe("ClientWalletForm", () => {
   afterEach(cleanup);
 
   const mockStore = configureStore([]);
-
-  const onSubmitMock = jest.fn();
 
   const initialState = { clientsWallet: [] };
   const store = mockStore(initialState);
@@ -36,23 +42,29 @@ describe("ClientWalletForm", () => {
     return render(
       <Provider store={store}>
         <FormProvider {...{ register, errors, handleSubmit }}>
-          <ClientWalletForm
-            onSubmit={onSubmitMock}
-            cancelOperation={() => {}}
-          />
+          <ClientWalletForm onSubmit={mockOnClick} cancelOperation={() => {}} />
         </FormProvider>
       </Provider>
     );
   };
 
-  test("should two input exists at screen", () => {
+  test("should two input and two buttons exists at screen", async () => {
     renderBy();
-
     const nameWalletInput = screen.getByLabelText(/Nombre de la cartera/i);
     const moneyAvaibleInput = screen.getByLabelText(/Cantidad de dinero/i);
+    const submitButton = await screen.findByRole("button", {
+      name: /Guardar/i
+    });
+    const cancelButton = await screen.findByRole("button", {
+      name: /cancelar/i
+    });
 
     expect(nameWalletInput).toBeInTheDocument();
     expect(moneyAvaibleInput).toBeInTheDocument();
+    await waitFor(() => {
+      expect(submitButton).toBeInTheDocument();
+      expect(cancelButton).toBeInTheDocument();
+    });
 
     expect(nameWalletInput).toHaveValue("");
     expect(moneyAvaibleInput).toHaveValue("");
@@ -60,7 +72,6 @@ describe("ClientWalletForm", () => {
 
   test("should input 'nameWallet' contain defaultValue", async () => {
     renderBy({ nameWallet: "cliente8" });
-
     const nameWalletInput = await screen.findByRole("textbox", {
       name: /Nombre de la cartera/i
     });
@@ -70,5 +81,3 @@ describe("ClientWalletForm", () => {
     });
   });
 });
-
-//     screen.debug();
